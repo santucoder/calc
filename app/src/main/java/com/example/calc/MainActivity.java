f2e96f9c536acd8baf39bcaa579e4f7d377c984d
santucoder <maitysantu410@gmail.com>
2022-06-16 20:40:26 +0530

    Square Calculator

---
+++ app/src/main/java/com/example/calc/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.calc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editnum;
+    TextView result;
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editnum  = (EditText) findViewById(R.id.editTextNumber);
+        result = (TextView) findViewById(R.id.textView2);
+        result.setVisibility(View.INVISIBLE);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String value= editnum.getText().toString();
+                int finalValue=Integer.parseInt(value);
+                int r= (int) squar(finalValue);
+                result.setText("Answer="+r);
+            }
+        });
+
+    }
+    double squar(double x)
+    {
+        result.setVisibility(View.VISIBLE);
+        return x*x;
+
+    }
+}
